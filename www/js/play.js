6378321c44efd8bcf1b5610d6f4f0fcd52d14ed2
Rees Payne <rplegos@gmail.com>
2016-02-20 11:42:41 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -14,12 +14,17 @@ DogeDodge.Play.prototype = {
 
   preload: function () {
     this.load.image('background','assets/GameBackground.png',320,568);
+    this.load.spritesheet('player','assets/Guy.Who.Dodges.Stuff.png',40,71,2);
   },
 
   create: function() {
     this.background = this.add.tileSprite(0,0,320,568,'background');
     this.background.autoScroll(-50,50);
     this.background.scale.set(1);
+    this.player = this.add.sprite(100,100,'player',5);
+    this.player.scale.set(1);
+    this.player.animations.add('eyes');
+    this.player.animations.play('eyes',2,true);
   },
 
   update: function() {
