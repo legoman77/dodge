6c3fd659f87c9339ddbe4925534dac4f0f5a717e
Rees Payne <rplegos@gmail.com>
2016-02-27 11:33:23 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -21,13 +21,24 @@ DogeDodge.Play.prototype = {
     this.background = this.add.tileSprite(0,0,320,568,'background');
     this.background.autoScroll(-50,50);
     this.background.scale.set(1);
-    this.player = this.add.sprite(100,100,'player',5);
-    this.player.scale.set(1);
-    this.player.animations.add('eyes');
-    this.player.animations.play('eyes',2,true);
+    this.background.smoothed = false;
+    this.dodger = this.add.sprite(100,100,'player',5);
+    this.dodger.anchor.set(0.5,0.5);
+    this.dodger.smoothed = false;
+    this.dodger.animations.add('eyes');
+    this.dodger.animations.play('eyes',2,true);
+    
+    // movement keys
+    this.cursors = game.input.keyboard.createCursorKeys();
   },
 
   update: function() {
+    if (this.cursors.left.isDown) {
+      this.dodger.x -= 10;
+    }
+    if (this.cursors.right.isDown) {
+    this.dodger.x += 10;
+    }
   }
 
 };
